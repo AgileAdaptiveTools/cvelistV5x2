ee7e89976d25dce8274fb3f85a4b9befd3470b3b
hkong <hkong@mitre.org>
2026-02-05 22:43:05 -0500

    change batch size and max to smaller numbers for testing script

--- .github/workflows/process_cves.sh
+++ .github/workflows/process_cves.sh
@@ -5,8 +5,8 @@
 # --- CONFIGURATION ---
 MASTER_LIST="all_cve_files.json"
 TALLY_FILE="tally.json"
-BATCH_SIZE=2000
-MAX_PER_DAY=35000
+BATCH_SIZE=20
+MAX_PER_DAY=350
 TODAY=$(date +%Y-%m-%d)
 
 # Fallback for local testing/GitHub Summary
